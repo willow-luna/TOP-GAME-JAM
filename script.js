52107f6553680272b5c31f22f250dc7e7dad293c
Melyssa <m.g.mayhem@gmail.com>
2021-07-23 13:32:39 -0400

    Add fun score emoji

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let round = 0;
 window.onload = firstQuestion();
 let star = "⭐";
+let cloud = "🌥️";
+let rainbow = "🌈";
+let dragon = "🐉";
+let unicorn = "🦄";
 let score = [];
+let bigScore = [];
 
 const numbers = document.querySelectorAll(".numberBtn");
     numbers.forEach((button) =>  button.addEventListener("click", answerQuestion));
@@ -9,103 +14,125 @@ const numbers = document.querySelectorAll(".numberBtn");
 document.getElementById("restart").addEventListener("click", restart);
 document.getElementById("quit").addEventListener("click", quit);
 
+let questions = [
+    {
+        questNum: "1",
+        quest : "What is 1 + 1?",
+        ans : "2"
+    },
+    {
+        questNum: "2",
+        quest : "What is 2 + 2?",
+        ans : "4",
+    },
+    {
+        questNum: "3",
+        quest: "Which is bigger, 1 or 2?",
+        ans : "2",
+    },
+    {
+        questNum: "4",
+        quest: "What comes AFTER 4?",
+        ans : "5",
+    },
+    {
+        questNum: "5",
+        quest: "What is 4 + 5?",
+        ans : "9",
+    },
+    {
+        questNum: "6",
+        quest: "What is 3 + 2?",
+        ans : "5",
+    },
+    {
+        questNum: "7",
+        quest: "What is 2 - 1?",
+        ans : "1",
+    },
+    {
+        questNum: "8",
+        quest: "What is 5 - 2?",
+        ans : "3",
+    },
+    {
+        questNum: "9",
+        quest: "What comes AFTER 7?",
+        ans : "8",
+    },
+    {
+        questNum: "10",
+        quest: "What comes AFTER 9?",
+        ans : "10",
+    },
+    {
+        questNum: "11",
+        quest: "What comes AFTER 2?",
+        ans : "3",
+    },
+    {
+        questNum: "12",
+        quest: "What comes BEFORE 3?",
+        ans : "2",
+    },
+    {
+        questNum: "13",
+        quest: "What is 9 + 1?",
+        ans : "10",
+    },
+    {
+        questNum: "14",
+        quest: "What is 9 - 3?",
+        ans : "6",
+    },
+    {
+        questNum: "15",
+        quest: "What is 10 - 5?",
+        ans : "5",
+    },
+    {
+        questNum: "16",
+        quest: "What is 8 - 6?",
+        ans : "2",
+    },
+    {
+        questNum: "17",
+        quest: "What is 1 + 9?",
+        ans : "10",
+    },
+    {
+        questNum: "18",
+        quest: "What is 5 - 4?",
+        ans : "1",
+    },
+    {
+        questNum: "19",
+        quest: "What is 7 + 3",
+        ans : "10",
+    },
+    {
+        questNum: "20",
+        quest: "What is 6 + 2?",
+        ans : "8",
+    },
+];
+
 
 function answerQuestion(e) {
     
     let answer = e.target.id.slice(3);
-        let questions = [
-            {
-                quest : "What is 1 + 1?",
-                ans : "2"
-            },
-            {
-                quest : "What is 2 + 2?",
-                ans : "4",
-            },
-            {
-                quest: "Which is bigger, 1 or 2?",
-                ans : "2",
-            },
-            {
-                quest: "What comes AFTER 4?",
-                ans : "5",
-            },
-            {
-                quest: "What is 4 + 5?",
-                ans : "9",
-            },
-            {
-                quest: "What is 3 + 2?",
-                ans : "5",
-            },
-            {
-                quest: "What is 2 - 1?",
-                ans : "1",
-            },
-            {
-                quest: "What is 5 - 2?",
-                ans : "3",
-            },
-            {
-                quest: "What comes AFTER 7?",
-                ans : "8",
-            },
-            {
-                quest: "What comes AFTER 9?",
-                ans : "10",
-            },
-            {
-                quest: "What comes AFTER 2?",
-                ans : "3",
-            },
-            {
-                quest: "What comes BEFORE 3?",
-                ans : "2",
-            },
-            {
-                quest: "What is 9 + 1?",
-                ans : "10",
-            },
-            {
-                quest: "What is 9 - 3?",
-                ans : "6",
-            },
-            {
-                quest: "What is 10 - 5?",
-                ans : "5",
-            },
-            {
-                quest: "What is 8 - 6?",
-                ans : "2",
-            },
-            {
-                quest: "What is 1 + 9?",
-                ans : "10",
-            },
-            {
-                quest: "What is 5 - 4?",
-                ans : "1",
-            },
-            {
-                quest: "What is 7 + 3",
-                ans : "10",
-            },
-            {
-                quest: "What is 6 + 2?",
-                ans : "8",
-            },
-        ];
+        
     document.getElementById("txtAns").textContent = answer;
     checkAnswer();
 
     function checkAnswer() {
         let qAns  = questions[round].ans;
-        console.log(qAns);
         if (answer === qAns) {
             document.getElementById("txtScore").textContent = "🎉 You got it right! Great Job! 🎉 ";
             setTimeout(updateQuestion, 2000);
             round++;
             updateScoreboard();
+            updateStars();
             
         }
         else {
@@ -120,19 +147,37 @@ function answerQuestion(e) {
         document.getElementById("txtScore").textContent = "How about this one?"
         txtQ.textContent = questions[round].quest;
         document.getElementById("txtAns").textContent = "?";
-        
-        console.log(txtQ.textContent);
     }
 
 };
 
 function updateScoreboard() {
-    score.push(star);
-    let newScore = score.join("");
+    
+    if(questions[round].questNum % 5 === 0){
+        score = [];
+            if(questions[round].questNum === "5") {
+                bigScore.push(cloud);
+            }
+            else if (questions[round].questNum === "10") {
+                bigScore.push(rainbow);
+            }
+            else if (questions[round].questNum === "15") {
+                bigScore.push(dragon);
+            }
+            else {
+                bigScore.push(unicorn);
+            }
+    }
+    else {
+        score.push(star);
+    }
+
+    let newScore = bigScore.join("") + score.join("");
     document.getElementById("numScore").textContent = "Score: " + newScore;
-    console.log(star);
-    console.log(score.join()=== score);
+    
 }
+
+
 
 function firstQuestion() {
     document.getElementById("txtQ").textContent = "What is 1 + 1?";
